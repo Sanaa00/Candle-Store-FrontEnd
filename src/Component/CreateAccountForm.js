0f96c239640bded35910b137d5c586a6003e22c3
Sana Rzgar <sanarizgar34@gmail.com>
2023-03-23 12:21:55 +0300

    form handlling & validation

--- src/Component/CreateAccountForm.js
+++ src/Component/CreateAccountForm.js
@@ -2,25 +2,90 @@ import React from "react";
 import InputField from "./InputField";
 import Button from "./Button";
 import { Link } from "react-router-dom";
+import { Formik, Form, Field, useFormik } from "formik";
+import * as Yup from "yup";
 function CreateAccountForm() {
+  const formik = useFormik({
+    initialValues: {
+      firstname: "",
+      lastname: "",
+      email: "",
+      password: "",
+      confirmpasssword: "",
+    },
+    onSubmit: (values) => {
+      console.log(values);
+    },
+    validationSchema: Yup.object({
+      firstname: Yup.string().label("First Name").required(),
+      lastname: Yup.string().label("Last Name").required(),
+      email: Yup.string().email().required(),
+      password: Yup.string()
+        .required("No password provided.")
+        .min(8, "Password is too short - should be 8 chars minimum."),
+      confirmpasssword: Yup.string().oneOf(
+        [Yup.ref("password"), null],
+        "Passwords must match"
+      ),
+    }),
+  });
   return (
-    <div className="grid grid-cols-1 mb-10">
-      <InputField placeholder="First Name" name="firstname" value="" />
-      <InputField placeholder="Last Name" name="lastname" value="" />
-      <InputField placeholder="Email" name="email" value="" />
-      <InputField placeholder="Password" name="password" value="" />
-      <InputField
-        placeholder="Confirm Password"
-        name="confirmpasssword"
-        value=""
-      />
-      <div className="mt-10 flex flex-col justify-center items-center">
-        <Button text="Order Now" width="96" />
-        <Link to="/login" className="mt-2">
-          Have Account? Login{" "}
-        </Link>
+    <Formik>
+      <div>
+        <Form onSubmit={formik.handleSubmit} className="grid grid-cols-1 mb-10">
+          <InputField
+            placeholder="First Name"
+            name="firstname"
+            id="firstname"
+            onChange={formik.handleChange}
+            value={formik.values.firstname}
+          />
+          <span className="text-red-400 text-sm">
+            {formik.errors.firstname}
+          </span>
+          <InputField
+            placeholder="Last Name"
+            name="lastname"
+            id="lastname"
+            onChange={formik.handleChange}
+            value={formik.values.lastName}
+          />
+          <span className="text-red-400 text-sm">{formik.errors.lastname}</span>
+          <InputField
+            placeholder="Email"
+            name="email"
+            id="email"
+            onChange={formik.handleChange}
+            value={formik.values.email}
+          />
+          <span className="text-red-400 text-sm">{formik.errors.email}</span>
+          <InputField
+            placeholder="Password"
+            name="password"
+            id="password"
+            onChange={formik.handleChange}
+            value={formik.values.password}
+          />
+          <span className="text-red-400 text-sm">{formik.errors.password}</span>
+          <InputField
+            placeholder="Confirm Password"
+            name="confirmpasssword"
+            id="confirmpassword"
+            onChange={formik.handleChange}
+            value={formik.values.confirmpassword}
+          />
+          <span className="text-red-400 text-sm">
+            {formik.errors.confirmpasssword}
+          </span>
+          <div className="mt-10 flex flex-col justify-center items-center">
+            <Button text="Order Now" width="96" type="submit" />
+            <Link to="/login" className="mt-2">
+              Have Account? Login{" "}
+            </Link>
+          </div>
+        </Form>{" "}
       </div>
-    </div>
+    </Formik>
   );
 }
 
---
+++ src/Component/InputField.js
@@ -0,0 +1,16 @@
+import React from "react";
+import { Field } from "formik";
+function InputField({ name, placeholder, value, onChange, id }) {
+  return (
+    <Field
+      placeholder={placeholder}
+      name={name}
+      value={value}
+      onChange={onChange}
+      id={id}
+      className="w-96 border-b-2 border-greeen focus:outline-none mt-10 px-1 py-1"
+    />
+  );
+}
+
+export default InputField;
