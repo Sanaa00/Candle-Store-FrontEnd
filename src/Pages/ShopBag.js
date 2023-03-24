f5c3296d4bc1ca13f150fadb482afbd0056c6ced
Sana Rzgar <sanarizgar34@gmail.com>
2023-03-24 21:43:38 +0300

    fixed css ...

--- src/Pages/ShopBag.js
+++ src/Pages/ShopBag.js
@@ -2,14 +2,22 @@ import React from "react";
 import Container from "../Component/Container";
 import ShoppingBagCard from "../Component/ShoppingBagCard";
 import TotalPrice from "../Component/TotalPrice";
+import { useSelector } from "react-redux";
 // import Data from "../Data";
 function ShopBag() {
+  const bag = useSelector((state) => state.candle.candle);
   return (
     <Container>
-      <p className="text-2xl font-semibold text-greeen my-5">Candles</p>
-      <div className="grid grid-cols-2 pag-5">
-        <ShoppingBagCard />
-        <TotalPrice />
+      <div className="h-screen">
+        <p className="text-2xl font-semibold text-greeen my-5">Candles</p>
+        {bag.length === 0 ? (
+          <p className=" text-gray-800 font-semibold mt-5">Your Bag is Empty</p>
+        ) : (
+          <div className="grid grid-cols-2 pag-5">
+            <ShoppingBagCard />
+            <TotalPrice />
+          </div>
+        )}{" "}
       </div>
     </Container>
   );
